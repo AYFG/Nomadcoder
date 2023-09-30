acc7beb749b85c0bb82b89ab811140a4d197cf29
AYFG <sek82468246@gmail.com>
2023-10-01 02:24:13 +0900

    delete btn 추가

--- todo.js
+++ todo.js
@@ -2,11 +2,25 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(e) {
+  //   console.log(e.composedPath()); //path가 없어서 composedPath 사용
+  //   console.log(e.target.parentElement); // del btn의 부모 li를 삭제
+  const li = e.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   //   console.log("i will paint", newTodo);
   const li = document.createElement("li");
   const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "Delete";
+  //   button.addEventListener("click", () => {
+  //     console.log("lalala");
+  //   });
+  button.addEventListener("click", deleteTodo);
   li.appendChild(span);
+  li.appendChild(button);
   span.innerText = newTodo;
   //   console.log(li);
   toDoList.appendChild(li);
